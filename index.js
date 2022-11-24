7f76439d4c0a6342d881dcf3556a8c0b1f22a2f6
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-11-24 16:34:38 +0600

    api for loading product categories

--- index.js
+++ index.js
@@ -18,7 +18,22 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9rpk71q.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
+async function run() {
+    try {
+        const UsedMobileDealCollection = client.db("UsedMobileDeal").collection("categoryList");
+        app.get('/phonecategory', async (req, res) => {
+            const query = {}
+            const result = await UsedMobileDealCollection.find(query).toArray();
+            res.send(result);
+            
+        })
+    }
+    finally {
+        
+    }
+}
 
+run().catch(err => console.log(err))
 
 
 app.get('/', async (req, res) => {
